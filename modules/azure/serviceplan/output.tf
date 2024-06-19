9c7e15dd9187b5ea9d65f3850a813e3db4cc366b
rveraromero <rveraromero@microsoft.com>
2024-06-19 16:24:44 -0600

    chore: Update Azure service plan configuration for dev environment

--- modules/azure/serviceplan/output.tf
+++ modules/azure/serviceplan/output.tf
@@ -1,9 +1,9 @@
 output "service_plan_id" {
   description = "The ID of the service plan"
-  value       = azapi_service_plan.service_plan.id
+  value       = azurerm_app_service_plan.service_plan.id
 }
 
 output "service_plan_name" {
   description = "The name of the service plan"
-  value       = azapi_service_plan.service_plan.name
+  value       = azurerm_app_service_plan.service_plan.name
 }
